5a84d5b28284620ce6202d22e258495d3d39ab6d
Erik <50298335+EriikP4@users.noreply.github.com>
2024-11-27 16:57:51 +0100

    Initial Files
    v1 of the Scripts

---
+++ english/script.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Enter the Mii's name
+set /p output_folder=
+if not exist miis\%output_folder% mkdir miis\%output_folder%
+
+echo Paste the URL of Mii Renderer
+set /p "base_url="
+
+cls
+echo ***         Summary         ***
+echo -------------------------------
+echo Mii: %output_folder%
+echo Rotation:  from 0 to 359 (full)
+echo -------------------------------
+echo Press ENTER to download
+pause >nul
+cls
+
+for /L %%i in (0,1,359) do (
+    echo Downloading rotation %%i...
+    set "url=%base_url%&characterYRotate=%%i"
+    curl -s "!url!" -o "miis\%output_folder%\mii_rotation_%%i.png"
+    if not exist "miis\%output_folder%\mii_rotation_%%i.png" (
+        echo Error downloading rotation %%i.
+    )
+)
+cls
+echo Download completed. The images are in the folder "miis\%output_folder%"
+echo Press any key to end the process
+pause >nul
+exit
---
+++ spanish/script.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Introduce el nombre del Mii
+set /p output_folder=
+if not exist %output_folder% mkdir %output_folder%
+
+echo Pega la URL de Mii Renderer
+set /p "base_url="
+
+cls
+echo ***         Resumen         ***
+echo -------------------------------
+echo Mii: %output_folder%
+echo Rotación: de 0 a 359 (completa)
+echo -------------------------------
+echo Puls ENTER para descargar
+pause >nul
+cls
+
+for /L %%i in (0,1,359) do (
+    echo Descargando rotación %%i...
+    set "url=%base_url%&characterYRotate=%%i"
+    curl -s "!url!" -o "miis\%output_folder%\mii_rotation_%%i.png"
+    if not exist "miis\%output_folder%\mii_rotation_%%i.png" (
+        echo Error al descargar la rotación %%i.
+    )
+)
+cls
+echo Descarga completada. Las imágenes están en la carpeta "miis\%output_folder%"
+echo Pulsa cualquier tecla para finalizar
+pause >nul
+exit
